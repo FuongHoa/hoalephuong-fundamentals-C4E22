4a6b031e1710572741a2481cc087288082e2ae93
Phuong Hoa <hoa.hlp00@gmail.com>
2018-10-06 23:36:09 +0700

    initial commit

--- session4/Homework/quiz1.py
+++ session4/Homework/quiz1.py
@@ -1,17 +1,14 @@
 print("Answer the following algebra question: ")
 print("If x = 8, then what is the value of 4 * (x + 3)?")
-choices = {
-    1: 35,
-    2: 36,
-    3: 40,
-    4: 44,
-}
-for k,v in choices.items():
-    print(k,v,sep =". ")
+quiz = {
+    "choices": ["1. 35", "2. 36", "3. 40", "4. 44"],
+    "right_choice": 4,
+}   
+print(*quiz["choices"], sep ="\n")
 
 answer = int(input("Your choice: "))
-ans = answer - 1
-if ans == 3:
+
+if answer == quiz["right_choice"]:
     print("Bingoooooooooooo")
 else:
     print(":(")
